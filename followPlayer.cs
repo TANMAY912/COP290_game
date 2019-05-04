4cc91ba3daf891273571d5a82eb35cb98782eea8
TANMAY912 <tanudim2012@gmail.com>
2019-05-04 18:36:57 +0530

    Add files via upload

---
+++ followPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+{
+    public Transform v;
+    public Vector3 offset = new Vector3(0, 2.0f, 3.0f);
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.position = v.position + offset;
+    }
+}
---
+++ movement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float ff = 4000f, sf = 120f;
+    public Vector3 dp;
+    public bool moveu = false, moved = false, movel = false, mover = false, jump = false;
+    void Start()
+    {
+        dp = transform.position;
+        //Debug.Log(dp);
+    }
+
+    void Update()
+    {
+        moveu = Input.GetKey("w");
+        movel = Input.GetKey("a");
+        moved = Input.GetKey("s");
+        mover = Input.GetKey("d");
+        //jump = Input.GetKey("m");
+    }
+    void FixedUpdate()
+    {
+       //    rb.AddForce(0, 0, ff * Time.deltaTime);
+            rb.AddForce(0,0,15 * Time.deltaTime, ForceMode.VelocityChange);
+        if (mover)
+        {
+            rb.AddForce(sf * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (movel)
+        {
+            rb.AddForce(-sf * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if(moveu)
+        {
+            rb.AddForce(0,0,sf * Time.deltaTime, ForceMode.VelocityChange);
+   //         rb.AddForce(0, 0, 500 * Time.deltaTime);
+        }
+        if(moved)
+        {
+            rb.AddForce(0,0,-sf * Time.deltaTime, ForceMode.VelocityChange);
+ //           rb.AddForce(0, 0, -500 * Time.deltaTime);
+        }
+    }
+}
